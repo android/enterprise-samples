98a8e8be71b1bac067dab86e9216f6e01f8a1c2e
Pietro F. Maggi <pmaggi@google.com>
2022-11-02 18:11:06 +0100

    Fix lint error for AppRestrictionSchema

--- AppRestrictionSchema/Application/src/main/java/com/example/android/common/logger/LogView.java
+++ AppRestrictionSchema/Application/src/main/java/com/example/android/common/logger/LogView.java
@@ -17,12 +17,12 @@ package com.example.android.common.logger;
 
 import android.app.Activity;
 import android.content.Context;
-import android.util.*;
-import android.widget.TextView;
+import android.util.AttributeSet;
+import androidx.appcompat.widget.AppCompatTextView;
 
 /** Simple TextView which is used to output log data received through the LogNode interface.
 */
-public class LogView extends TextView implements LogNode {
+public class LogView extends AppCompatTextView implements LogNode {
 
     public LogView(Context context) {
         super(context);
